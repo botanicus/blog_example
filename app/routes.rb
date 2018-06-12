58ee9e0e99b19906a872af540781d7b92193e597
James C Russell <james@101ideas.cz>
2018-06-12 10:43:44 +0200

    Rubocop rule Style/ExpandPathArguments

--- app/routes.rb
+++ app/routes.rb
@@ -5,7 +5,7 @@ logger, router = import('registry').grab(:logger, :empty_router)
 router.register_default(import('routes/errors/404'))
 router.register_error_route(import('routes/errors/500'))
 
-route_dir = File.expand_path('../routes', __FILE__)
+route_dir = File.expand_path('routes', __dir__)
 Dir.glob("#{route_dir}/*.rb").each do |path|
   logger.debug("Registering routes from #{path.sub("#{Dir.pwd}/", '')}.")
   exported_route = import(path)
