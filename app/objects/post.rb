aa7dd497d7335c72f4c247f095cc5726c8468280
James C Russell <james@101ideas.cz>
2018-06-12 10:23:46 +0200

    Rubocop rule Layout/LeadingCommentSpace

--- app/objects/post.rb
+++ app/objects/post.rb
@@ -2,12 +2,12 @@
 
 class Post
   attr_reader :data
-  def initialize(id: nil, title:, body:)#, author:, tags: Array.new)
-    @data = {id: id, title: title, body: body}#, author: author, tags: tags}
+  def initialize(id: nil, title:, body:)# , author:, tags: Array.new)
+    @data = {id: id, title: title, body: body}# , author: author, tags: tags}
   end
 
   def reload(id: nil, title:, body:)
-    @data = {id: id, title: title, body: body}#, author: author, tags: tags}
+    @data = {id: id, title: title, body: body}# , author: author, tags: tags}
   end
 end
 
